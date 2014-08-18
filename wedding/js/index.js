150ff2df40aeacd9ffeb2e0498b3947887dc9052
chenmiaoqing <cmq7721454@163.com>
2014-08-18 17:01:26 +0800

    2014-8-18 afternoon
    1.搭建wedding部分框架
    2.熟练使用幻灯片
    3.熟悉animate

--- wedding/js/index.js
+++ wedding/js/index.js
@@ -1,17 +1,39 @@
 $(function(){
 
 	showFlash();
-	timerHandle= setInterval( "showFlash()",2000);
+	timerHandle= setInterval( "showFlash()",3000);
+
+ 	$("#heart").click(function(){
+ 		//alert("a");
+
+		var isshow = $("#menu").attr("isshow");
+
+		if(isshow=="no"){
+			$("#menumain").animate({ "left":"1090px"},0);
+			
+			$("#menu").attr("isshow","yes");
+			
+		}
+		else{
+			$("#menumain").animate({ "left":"1290px"},0);
+			
+			$("#menu").attr("isshow","no");
+			
+		}
+	});
+
+
 });
 
 
 
 var timerHandle;
-var currentindex=2;
+var currentindex=1;
 function showFlash(){
-	
-	$("#flash li").css("background-image",'url("images/flash/slider-'+currentindex+'.jpg")');
-									
+	 $(".opacity").css("opacity","0");
+	 $(".opacity"+currentindex).css("opacity","1");
+	 
+						
 	currentindex++;
 	if(currentindex==4){
 		currentindex=1;
