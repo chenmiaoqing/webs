9d1afcb57427493f9a387f62eaa7d8907533b935
chenmiaoqing <cmq7721454@163.com>
2014-08-22 17:17:04 +0800

    2014-8-22 afternoon
    weibo回复功能

---
+++ taobao/js/index.js
@@ -0,0 +1,32 @@
+$(function(){
+      $(".menubdA").hover(function(){
+              $(".menubdAmore").show();
+      },function(){
+              $(".menubdAmore").hide();
+      });
+      $(".getmenu").hover(function(){
+              $(".getxinfengmore").show();
+      },function(){
+              $(".getxinfengmore").hide();
+      });
+      $(".mytaobao").hover(function(){
+             $(".menuhdr1").show();
+      },function(){
+              $(".menuhdr1").hide();
+      });
+       $(".gouwuche").hover(function(){
+             $(".menuhdr2").show();
+      },function(){
+              $(".menuhdr2").hide();
+      });
+       $(".shoucangjia").hover(function(){
+             $(".menuhdr3").show();
+      },function(){
+              $(".menuhdr3").hide();
+      });
+       $(".maijia").hover(function(){
+             $(".menuhdr4").show();
+      },function(){
+              $(".menuhdr4").hide();
+      });
+});
--- weibo.com/js/weibo.js
+++ weibo.com/js/weibo.js
@@ -77,13 +77,28 @@ $(function(){
 	});
 
 
-	// //随着滚动条变化LeftAll2固定
-	// $(window).srcoll(function(){
-	// 	var scrolls=$(this).scrollTop();
-	// 	if(scrolls>45){
-	// 		$("#LeftAll2").css("position","fixed");
-	// 	}
-	// });
+	//随着滚动条变化LeftAll2 remenhuati固定
+	$(window).scroll(function(){
+
+		var scrolls = $(this).scrollTop();
+		// alert(scrolls);
+		if(scrolls>600  & scrolls<3800){
+			$("#LeftAll2").css({"position":"fixed","top":"0px"});
+		}else{
+			$("#LeftAll2").css("position","");
+		}
+	});
+
+	$(window).scroll(function(){
+
+		var scrolls = $(this).scrollTop();
+		// alert(scrolls);
+		if(scrolls>1300 & scrolls<3600){
+			$("#remenhuati").css({"position":"fixed","top":"60px"});
+		}else{
+			$("#remenhuati").css("position","");
+		}
+	});
 	
 
 	//日期选择器
@@ -147,33 +162,225 @@ $(function(){
 	});
 
 
-	$(".replyed dl").hover(function(){
-		$(this).find(".hover").css("opacity","1");
-	},function(){
-		$(this).find(".hover").css("opacity","0");
+	// $(".replyed dl").hover(function(){
+	// 	$(this).find(".hover").css("opacity","1");
+	// },function(){
+	// 	$(this).find(".hover").css("opacity","0");
+	// });
+
+	// $(".pinglun").click(function(){
+	// 	var isshow=$(this).attr("isshow");
+	// 	if(isshow=="no"){
+	// 		$(this).parent().parent().next(".reply").show();
+	// 		$(this).attr("isshow","yes");
+	// 	}else{
+	// 		$(this).parent().parent().next(".reply").hide();
+	// 		$(this).attr("isshow","no");
+	// 	}	
+	// });
+
+	$(document).on("click",".huifu",function(){
+		replySon($(this));
+	});
+	$(document).on("click",".replyBRight",function(){
+		tijiao($(this));
 	});
 
-	$(".pinglun").click(function(){
-		var isshow=$(this).attr("isshow");
-		if(isshow=="no"){
-			$(this).parent().parent().next(".reply").show();
-			$(this).attr("isshow","yes");
-		}else{
-			$(this).parent().parent().next(".reply").hide();
-			$(this).attr("isshow","no");
-		}
 
-		
-	});
+	reply();
 
-
-
-
-
-
+	
+	
 
 });
 
+
+function reply(){
+
+	
+
+	$(".pinglun").click(function(){
+		var talkid=$(this).attr("talkid");
+		var isshow=$(this).attr("isshow");	
+		if(isshow=="no"){
+			
+			// $(this).parent().parent().next(".reply").show();
+			var isshown=$("#"+talkid).attr("iscunzai");
+		
+			if(isshown!=null)
+			{
+				
+				$("#"+talkid).show();
+
+				$(".replyed dl").hover(function(){
+					//alert("a");
+					$(this).find(".hover").css("opacity","1");
+				},function(){
+					$(this).find(".hover").css("opacity","0");
+				});
+
+			}
+			else
+			{
+				var replyA='';
+		
+				replyA +=' <div id="'+ talkid +'" iscunzai="yes"  class="reply">';
+				replyA +=' 		<div>';
+				replyA +='			<!-- 评论框 -->';
+				replyA +='			<textarea class="textarea" id="sp_1"></textarea>';
+				replyA +='			<ul>';
+				replyA +='				<li class="replyBLeft">';
+				replyA +='					<span></span>';
+				replyA +='					<a><input type="checkbox">同时转发到我的微博</a>';
+				replyA +='				</li>';
+				replyA +='				<li class="replyBRight">';
+				replyA +='					<a><span>';
+				replyA +='						<em>评论</em>';
+				replyA +='					</span></a>';
+				replyA +='				</li>';
+				replyA +='			</ul>';
+				replyA +='			<!-- 已有的评论列表 -->';
+				replyA +='			<div class="replyed">';
+				replyA +='				<dl>';
+				replyA +='					<dt><a><img src="images/UserHeadImages/3.jpg" class="replyImg"></a></dt>';
+				replyA +='					<dd>';
+				replyA +='						<a>-春天藥丸-</a>：…… (11分钟前)';
+				replyA +='						<div class="info">';
+				replyA +='							<span>';
+				replyA +='								<em class="hover"><a>举报</a><span>|</span></em>';
+				replyA +='								<a><i></i></a>';
+				replyA +='								<span>|</span>';
+				replyA +='								<a class="huifu" isshow="no" talkSonid="son1">回复</a>';
+				replyA +='							</span>';
+				replyA +='						 </div>  ';
+				replyA +='						 <div class="replySonContent" id="replySonContentson1">'
+				replyA +='						</div>'
+				replyA +='					</dd>';
+				replyA +='				</dl>';
+				replyA +='				<dl>';
+				replyA +='					<dt><a><img src="images/UserHeadImages/3.jpg" class="replyImg"></a></dt>';
+				replyA +='					<dd>';
+				replyA +='						<a>-春天藥丸-</a>：…… (11分钟前)';
+				replyA +='						<div class="info">';
+				replyA +='							<span>';
+				replyA +='								<em class="hover"><a>举报</a><span>|</span></em>';
+				replyA +='								<a><i></i></a>';
+				replyA +='								<span>|</span>';
+				replyA +='								<a class="huifu" isshow="no" talkSonid="son2">回复</a>';
+				replyA +='							</span>';
+				replyA +='						</div>   ';
+				replyA +='						 <div class="replySonContent" id="replySonContentson2">'
+				replyA +='						</div>'
+				replyA +='					</dd>';
+				replyA +='				</dl>';
+				replyA +='			</div>';
+				replyA +='		</div>';
+				replyA +='	</div>';
+
+
+				$("#replyContent"+talkid).append(replyA);
+				$(".replyed dl").hover(function(){
+					//alert("b");
+					$(this).find(".hover").css("opacity","1");
+				},function(){
+					$(this).find(".hover").css("opacity","0");
+				});
+			}
+			
+			
+			
+
+		$(this).attr("isshow","yes");
+
+	}
+
+
+		else{
+			// $(this).parent().parent().next(".reply").hide();
+			$("#"+talkid).hide();
+			$(this).attr("isshow","no");
+		}	
+
+	});
+
+
+}
+
+function tijiao(t){
+		var replyContentHtml ="";
+		var sp_1=document.getElementById("sp_1");
+		var MyWords=sp_1.value;
+		//alert(MyWords);
+		
+		replyContentHtml +='				<dl>';
+		replyContentHtml +='					<dt><a><img src="images/UserHeadImages/4.jpg" class="replyImg"></a></dt>';
+		replyContentHtml +='					<dd>';
+		replyContentHtml +='						<a>茉香半糖</a>：'+ MyWords+' ';
+		replyContentHtml +='						<div class="info">';
+		replyContentHtml +='							<span>';
+		replyContentHtml +='								<em class="hover"><a>举报</a><span>|</span></em>';
+		replyContentHtml +='								<a><i></i></a>';
+		replyContentHtml +='								<span>|</span>';
+		replyContentHtml +='								<a class="huifu" isshow="no" talkSonid="son1">回复</a>';
+		replyContentHtml +='							</span>';
+		replyContentHtml +='						 </div>  ';
+		replyContentHtml +='						 <div class="replySonContent" id="replySonContentson1">'
+		replyContentHtml +='						</div>'
+		replyContentHtml +='					</dd>';
+		replyContentHtml +='				</dl>';
+		//alert(replyContentHtml);
+
+		
+		 $(".replyed").append(replyContentHtml);
+		
+}
+
+function replySon(e){
+	
+			var talkSonid=e.attr("talkSonid");
+			var isshow=e.attr("isshow");
+			if(isshow=="no"){
+				
+				
+				var isshown=e.attr("iscunzai");
+
+				if(isshown!=null){
+					$("#"+talkSonid).show();
+				}
+				else
+				{ 
+					var replySonA='';
+					 replySonA	+='<div class="replySon" id="'+ talkSonid +'"  iscunzai="yes">';
+					 replySonA	+='		<div>';
+					 replySonA	+='	 		<textarea class="textarea"></textarea>';
+					 replySonA	+='			<ul>';
+					 replySonA	+='				<li class="replySonBLeft">';
+					 replySonA	+='					<span></span>';
+					 replySonA	+='					<a><input type="checkbox">同时转发到我的微博</a>';
+					 replySonA	+='				</li>';
+					 replySonA	+='				<li class="replySonBRight">';
+					 replySonA	+='					<a><span>';
+					 replySonA	+='						<em>评论</em>';
+					 replySonA	+='						</span></a>';
+					 replySonA	+='				</li>';
+					 replySonA	+='			</ul>';
+					 replySonA	+='		</div>';
+					 replySonA	+='</div>   ';
+					
+					 $("#replySonContent"+talkSonid).append(replySonA);
+
+
+
+				}
+				e.attr("isshow","yes");
+			}else{
+				$("#"+talkSonid).hide();
+				e.attr("isshow","no");
+			}
+		
+
+
+	}
 
 
 
